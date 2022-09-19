05339107181c06c946631665531966d043fa27bc
IvanovRoman <meninyellow8@gmail.com>
2022-09-19 23:57:51 +0300

    Program created

---
+++ Program/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] FindElementsOfLength3OrLess(string[] array)
+{
+    string[] newArray = new string[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Array.Resize(ref newArray, newArray.Length + 1);
+            newArray[newArray.Length - 1] = array[i];
+        }
+    }
+    return newArray;
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+
+string[] arr = new string[1] { "123" };
+var result = FindElementsOfLength3OrLess(arr);
+PrintArray(result);
